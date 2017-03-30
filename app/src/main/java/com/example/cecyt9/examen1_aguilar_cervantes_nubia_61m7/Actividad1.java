4e9357f0ada4d95a021f7f1c7bed0c044fb999c4
Nube99 <nubiaa40@gmail.com>
2017-03-30 16:03:17 -0600

    Examen1

--- app/src/main/java/com/example/cecyt9/examen1_aguilar_cervantes_nubia_61m7/Actividad1.java
+++ app/src/main/java/com/example/cecyt9/examen1_aguilar_cervantes_nubia_61m7/Actividad1.java
@@ -3,31 +3,31 @@ package com.example.cecyt9.examen1_aguilar_cervantes_nubia_61m7;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Actividad1 extends AppCompatActivity {
 
     EditText numero;
     int numeroC=0;
-    String resultado= "Resultado: ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad1);
+        numero = (EditText) findViewById(R.id.Cantidad);
     }
 
-    public void onClickConvertir(){
-
-        numero = (EditText) findViewById(R.id.Cantidad);
-        numeroC = (Integer.parseInt(numero.getText().toString())/15);
-        resultado = resultado + numeroC;
-        Intent envia = new Intent(this, Actividad2.class);
+    public void onClick(View vista){
+        numeroC = Integer.parseInt(numero.getText().toString());
+        Toast.makeText(getApplicationContext(),""+numeroC/15, Toast.LENGTH_LONG).show();
+        Intent envia = new Intent(this,Actividad2.class);
         Bundle datos = new Bundle();
-        datos.putString("resultado",resultado);
+        datos.putString("resultado",""+numeroC/15);
+        envia.putExtras(datos);
         finish();
         startActivity(envia);
-
     }
 
 }
--- app/src/main/java/com/example/cecyt9/examen1_aguilar_cervantes_nubia_61m7/Actividad2.java
+++ app/src/main/java/com/example/cecyt9/examen1_aguilar_cervantes_nubia_61m7/Actividad2.java
@@ -1,14 +1,40 @@
 package com.example.cecyt9.examen1_aguilar_cervantes_nubia_61m7;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Actividad2 extends AppCompatActivity {
+
+    TextView Respuesta;
+    String resultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad2);
+
+        Respuesta = (TextView) findViewById(R.id.Respuesta);
+       Intent recibe = getIntent();
+      Bundle Datos = recibe.getExtras();
+
+        resultado = Datos.getString("resultado");
+        Respuesta.setText("Nubia Aguilar Cervantes     Resultado: " + resultado);
+
     }
+    public void onClickEnvia(View vista){
+
+        Intent intencion =new Intent(Intent.ACTION_SEND);
+        intencion.setType("text/plain");
+        intencion.putExtra(Intent.EXTRA_SUBJECT, "Asunto: Examen 1er parcial ");
+        intencion.putExtra(Intent.EXTRA_TEXT, "Contenido del correo: " + Respuesta.getText().toString());
+        intencion.putExtra(Intent.EXTRA_EMAIL, new String[] { "nubia-04@live.com.mx"} );
+        startActivity(intencion);
+
+
+    }
+
 
 }
